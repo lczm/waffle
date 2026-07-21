60255d062f314b77b0f204d13425b96c893fd458
lczm <zemingchua@gmail.com>
2026-07-21 20:04:21 +0800

    support \r whitespace and add some tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,7 +1,7 @@
 use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq, Clone)]
-#[logos(skip r"[ \t\n\f]+")]
+#[logos(skip r"[ \t\r\n\f]+")]
 pub(crate) enum Token {
     #[token("(")]
     LParen,
@@ -37,4 +37,54 @@ pub(crate) enum Token {
         s[1..s.len()-1].to_string()
     })]
     String(String),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn lex(source: &str) -> Result<Vec<Token>, ()> {
+        Token::lexer(source).collect()
+    }
+
+    #[test]
+    fn skips_each_supported_whitespace_character() {
+        for space in [" ", "\t", "\r", "\n", "\x0c"] {
+            // left{space}{right} -> left right
+            let test_source = format!("left{space}right");
+
+            assert_eq!(
+                lex(&test_source),
+                Ok(vec![
+                    Token::Symbol("left".into()),
+                    Token::Symbol("right".into()),
+                ]),
+                "failed to skip whitespace {space:?}",
+            );
+        }
+    }
+
+    #[test]
+    fn skips_windows_crlf_line_endings() {
+        let source = "(\r\nadd\r\n\t1\r\n2\r\n)\r\n";
+
+        assert_eq!(
+            lex(source),
+            Ok(vec![
+                Token::LParen,
+                Token::Symbol("add".into()),
+                Token::Integer(1),
+                Token::Integer(2),
+                Token::RParen,
+            ])
+        );
+    }
+
+    #[test]
+    fn preserves_whitespace_inside_strings() {
+        assert_eq!(
+            lex("\"first line\r\nsecond line\""),
+            Ok(vec![Token::String("first line\r\nsecond line".into())])
+        );
+    }
 }
